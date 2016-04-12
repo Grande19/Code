91d57651092b6b9e3229665c49f46defc550391a
grande19 <1b15agc@gmail.com>
2016-04-12 10:55:20 +0200

    Comentado en clase

--- XML/xml-parser-jokes.py
+++ XML/xml-parser-jokes.py
@@ -9,7 +9,7 @@
 #
 # Just prints the jokes in a JokesXML file
 
-from xml.sax.handler import ContentHandler
+from xml.sax.handler import ContentHandler #manejador de contenido
 from xml.sax import make_parser
 import sys
 import string
@@ -18,26 +18,26 @@ def normalize_whitespace(text):
     "Remove redundant whitespace from a string"
     return string.join(string.split(text), ' ')
 
-class CounterHandler(ContentHandler):
+class CounterHandler(ContentHandler): #hereda del manejador generico
 
     def __init__ (self):
-        self.inContent = 0
-        self.theContent = ""
+        self.inContent = 0     #si estoy dentro del contenido que me interesa
+        self.theContent = ""   #maneja el contenido que me interesa
 
-    def startElement (self, name, attrs):
+    def startElement (self, name, attrs): #Se activa cuando empiezo a reconocer un elemento XML
         if name == 'joke':
             self.title = normalize_whitespace(attrs.get('title'))
-            print " title: " + self.title + "."
+            print " title: " + self.title + "." #escribe en pantalla el atributo tittle
         elif name == 'start':
-            self.inContent = 1
+            self.inContent = 1 #estoy en contenido
         elif name == 'end':
-            self.inContent = 1
-            
-    def endElement (self, name):
+            self.inContent = 1  #esto en contenido
+
+    def endElement (self, name): #cuando acabe de leer el final de un elmento
         if self.inContent:
             self.theContent = normalize_whitespace(self.theContent)
         if name == 'joke':
-            print ""
+            print "" #linea en blanco
         elif name == 'start':
             print "  start: " + self.theContent + "."
         elif name == 'end':
@@ -45,11 +45,11 @@ class CounterHandler(ContentHandler):
         if self.inContent:
             self.inContent = 0
             self.theContent = ""
-        
-    def characters (self, chars):
+
+    def characters (self, chars): #cuando acabe de leer los caracteres de un elemento
         if self.inContent:
-            self.theContent = self.theContent + chars
-            
+            self.theContent = self.theContent + chars #lo aniado al contenido
+
 # --- Main prog
 
 if len(sys.argv)<2:
@@ -57,7 +57,7 @@ if len(sys.argv)<2:
     print
     print " <document>: file name of the document to parse"
     sys.exit(1)
-    
+
 # Load parser and driver
 
 JokeParser = make_parser()
